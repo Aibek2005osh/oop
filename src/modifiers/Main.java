16e24fb06597921289a2dd75975a0dc6b83554fa
Aibek Dev <clesh.meder.271@gmail.com>
2024-11-13 15:32:07 +0600

    Modifiers

--- src/modifiers/Main.java
+++ src/modifiers/Main.java
@@ -10,11 +10,9 @@ public class Main {
         System.out.print("Radius : ");
       int radius = new Scanner(System.in).nextInt();
 
-        // Айлананын аянтын эсептөө
         double area = Circle.area(radius);
         System.out.println("Айлананын аянты: " + area);
 
-        // Айлананын узундугун эсептөө
         double circumference = Circle.circumference(radius);
         System.out.println("Айлананын узундугу: " + circumference);   
     }
